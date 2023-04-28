4bc4c4ac42f3d32b16fe906542a28325d07414dd
git config --global user.name Fazil <105953406+mu-fazil-vk@users.noreply.github.com>
2023-04-28 10:35:03 +0530

    change model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import replicate from 'node-replicate'
 
-const model = 'tstramer/midjourney-diffusion:436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b';
+const model = 'ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f';
 
 export default async (prompt, parameters) => 
 	await replicate.run(model, { prompt, ...parameters })
